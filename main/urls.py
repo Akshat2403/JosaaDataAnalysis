ae7c8f1ebba1d0d96f287c57433cb7ea0fe527c4
mohit <mohitsharma55337@gmail.com>
2023-07-03 17:48:04 +0530

    popularity added

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,8 @@ urlpatterns = [
     path('',views.upload_csv),
     path('data',views.printdata,name = 'data'),
     path('dig-q1',views.dig_q1,name = 'dig_q1'),
+    path('mohit-q1',views.Moh_q1,name = 'Moh_q1'),
+    path('mohit-q1exp',views.Moh_q1exp,name = 'Moh_q1exp'),
+    path('mohit-q1exp2',views.Moh_q1exp2,name = 'Moh_q1exp2'),
+    path('mohit-q2',views.branch_popularity,name = 'Moh_q2'),
 ]
