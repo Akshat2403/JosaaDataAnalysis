0b1581012c2c135846f2d601bae3881a452892c0
Akshat2403 <95174507+Akshat2403@users.noreply.github.com>
2023-07-05 16:46:15 +0530

    update legend

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.dashboard),
-    path('csv',views.upload_csv),
-    path('base',views.base,name='base'),
+    # path('csv',views.upload_csv),
+    # path('base',views.base,name='base'),
     path('data',views.printdata,name = 'data'),
     path('dig-q1',views.dig_q1,name = 'dig_q1'),
     path('sid-q1',views.sid_q1,name = 'sid_q1'),
